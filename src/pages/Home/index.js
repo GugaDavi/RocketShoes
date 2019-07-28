20ccb314436560d88a469470c65eb282dfa593e7
GugaDavi <gguedes.tech@gmail.com>
2019-07-28 00:32:12 -0300

    Estrutura e Styles da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function Home() {
-  return <h1>Home</h1>;
+import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
+
+export default class Cart extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(item => (
+          <li key={item.id}>
+            <img src={item.image} alt={item.title} />
+            <strong>{item.title}</strong>
+            <span>{item.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />
+              </div>
+              <span>Adicionar ao Carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
